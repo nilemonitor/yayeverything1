b1a53b47dadad6b238dca983128da70366ad5d59
Christine Lindgren Myers <nilemonitor@gmail.com>
2013-09-02 12:18:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yayeverything::Application.config.secret_token = 'c6b001c4e396b2f5daada4552de39eb32955386112907d0b7fd3b550e44e977e215ffbf2ae324baa71796c9bed88a650118cf5779f6e3d270239167b420a3c24'
